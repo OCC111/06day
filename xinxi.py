38d5a86f8dfe6beaa2e23e3ab1a4804cfd51b5bb
OCC111 <859899882@qq.com>
2018-06-07 11:02:08 +0800

    hello

---
+++ xinxi.py
@@ -0,0 +1,37 @@
+import time
+
+print('')
+time.sleep(1)
+print('>'*30)
+time.sleep(1)
+print('>    welcome to my school    >')
+time.sleep(1)
+print('>'*30)
+print('')
+
+time.sleep(2)
+name = input('please input you name:')
+time.sleep(2)
+age = str(input('please input you age:'))
+time.sleep(2)
+job = input('please input you job:')
+time.sleep(2)
+mail = input('please input you mail:')
+time.sleep(2)
+tel = int(input('please input you tel:'))
+print('')
+print('$'*30)
+print('')
+print('you input name is %s'% name)
+print('you input age is %s'% age)
+print('you input job is %s'% job)
+print('you input mail is %s'% mail)
+print('youinput tel is %d'% tel)
+
+
+
+
+
+
+
+
